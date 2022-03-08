a63f6bb4b2d3869587a23273e227c812c7e91bd0
Seojin Oh <seojinoh.me@gmail.com>
2022-03-09 00:03:00 +0900

    feat : Add Spring Boot project

---
+++ src/main/java/me/seojinoh/learning/springSecurityJwt/LearningSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package me.seojinoh.learning.springSecurityJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/seojinoh/learning/springSecurityJwt/LearningSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seojinoh.learning.springSecurityJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
