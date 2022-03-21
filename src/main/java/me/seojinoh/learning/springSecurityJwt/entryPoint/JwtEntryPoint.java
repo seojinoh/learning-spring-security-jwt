6d835c8435e3a7cea84e136fc492ac7c1cf9bd77
Seojin Oh <seojinoh.me@gmail.com>
2022-03-21 22:52:05 +0900

    feat : Update JwtEntryPoint to fit into response format

--- src/main/java/me/seojinoh/learning/springSecurityJwt/entryPoint/JwtEntryPoint.java
+++ src/main/java/me/seojinoh/learning/springSecurityJwt/entryPoint/JwtEntryPoint.java
@@ -7,18 +7,23 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import me.seojinoh.learning.springSecurityJwt.util.ResponseUtil;
 
 @Component
 public class JwtEntryPoint implements AuthenticationEntryPoint {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+	@Autowired private ResponseUtil responseUtil;
+
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException) throws IOException {
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized request");
+		responseUtil.setResponse(response, HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized", null);
 	}
 
 }
