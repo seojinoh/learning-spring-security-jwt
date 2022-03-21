e273000470ae93ee612519a32311eebc5c1e95f4
Seojin Oh <seojinoh.me@gmail.com>
2022-03-21 22:48:53 +0900

    feat : Add another setResponse method to ResponseUtil

--- src/main/java/me/seojinoh/learning/springSecurityJwt/util/ResponseUtil.java
+++ src/main/java/me/seojinoh/learning/springSecurityJwt/util/ResponseUtil.java
@@ -1,21 +1,40 @@
 package me.seojinoh.learning.springSecurityJwt.util;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import me.seojinoh.learning.springSecurityJwt.dto.CustomResponse;
 
 @Component
 public class ResponseUtil {
 
+	@Autowired private ObjectMapper objectMapper;
+
 	@SuppressWarnings("deprecation")
 	public void setResponse(HttpServletResponse response, CustomResponse customResponse, int status, String message, Object data) {
+		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 		response.setStatus(status, message);
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
 		customResponse.setStatus(status);
 		customResponse.setMessage(message);
 		customResponse.setData(data);
+	}
+
+	@SuppressWarnings("deprecation")
+	public void setResponse(HttpServletResponse response, int status, String message, Object data) throws IOException {
+		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		response.setStatus(status, message);
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.getWriter().write(objectMapper.writeValueAsString(new CustomResponse(status, message, data)));
 	}
 
 }
