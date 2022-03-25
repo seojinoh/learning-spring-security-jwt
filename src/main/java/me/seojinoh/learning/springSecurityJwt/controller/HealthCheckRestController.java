abfe6abc6244aae42138150781864ef5ed42975f
Seojin Oh <seojinoh.me@gmail.com>
2022-03-25 12:59:22 +0900

    feat : Update user/admin role test GET API

--- src/main/java/me/seojinoh/learning/springSecurityJwt/controller/HealthCheckRestController.java
+++ src/main/java/me/seojinoh/learning/springSecurityJwt/controller/HealthCheckRestController.java
@@ -1,14 +1,22 @@
 package me.seojinoh.learning.springSecurityJwt.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import me.seojinoh.learning.springSecurityJwt.util.ResponseUtil;
 
 @RestController
 public class HealthCheckRestController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired private ResponseUtil responseUtil;
 
 	@GetMapping("/ping")
 	public String ping() {
@@ -16,13 +24,13 @@ public class HealthCheckRestController {
 	}
 
 	@GetMapping("/user/test")
-	public String testUser() {
-		return "success";
+	public void testUser(HttpServletRequest request, HttpServletResponse response) {
+		responseUtil.setResponse(response, HttpServletResponse.SC_OK, "OK", null);
 	}
 
 	@GetMapping("/admin/test")
-	public String testAdmin() {
-		return "success";
+	public void testAdmin(HttpServletRequest request, HttpServletResponse response) {
+		responseUtil.setResponse(response, HttpServletResponse.SC_OK, "OK", null);
 	}
 
 }
